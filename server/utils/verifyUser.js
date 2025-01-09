d9a5cc8c31a596f9c63315c022924ecaa5d88673
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2025-01-09 23:48:06 +0530

    update user verification

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -2,12 +2,18 @@ import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) return next(errorHandler(401, 'You are not authenticated!'));
+  const authHeader = req.headers.authorization;
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return next(errorHandler(401, 'You are not authenticated!'));
+  }
 
-  jwt.verify(token, 'sjss', (err, user) => {
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, 'Token is not valid!'));
     req.user = user;
     next();
   });
 };
+
