0583b4485fb5ae1f5a5d0dd6860ff946f61e4579
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2025-01-09 19:28:10 +0530

    add build

--- server/index.js
+++ server/index.js
@@ -23,6 +23,13 @@ mongoose
 
 const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  });
+}
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
